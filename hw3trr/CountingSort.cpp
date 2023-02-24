9c1ad4ba4b6054ae9b91728dda2e08317b6e33ea
Hung Le <117164213+lequanghung17@users.noreply.github.com>
2023-02-24 12:09:46 +0700

    Update CountingSort.cpp

--- hw3trr/CountingSort.cpp
+++ hw3trr/CountingSort.cpp
@@ -5,7 +5,7 @@ void countingSort(int arr[100], int n)
   {
     int count[100];  //khởi tạo mảng mà mỗi phần tử là số lần xuất hiện của giá trị chỉ số
     //cho tất cả các phần tử của mảng count = 0, nghĩa là ban đầu chưa có số nào xuất hiện trong mảng
-    for (int i = -100; i <= 100; i++) {
+    for (int i = 0; i <= 100; i++) {
         count[i] = 0;
     }
     //chạy mảng arr và đếm dần số lần xuất hiện của các số trong mảng, của số arr[i] thì sẽ có count[arr[i]] lần xuất hiện
@@ -13,7 +13,7 @@ void countingSort(int arr[100], int n)
         count[arr[i]]++;
     }
     // chạy mảng count, count[arr[i]] > 0 thì đưa ra arr[i] luôn vì chắc chắn arr[i] đó là bé hơn do i chạy từ min đến max
-    for (int i = -100; i <= 100; i++) {
+    for (int i = 0; i <= 100; i++) {
         while (count[i] > 0) {
             cout << i << " ";
             count[i]--;
